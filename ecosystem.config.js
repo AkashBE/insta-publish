f2fdaec1abc8928fe6e93ff2dcd09addebb20b96
Akash Pradhan <akash@Akashs-MacBook-Air.local>
2024-06-30 18:13:37 +0530

    add pm2 manager

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+        {
+            name: 'instagram-publish',
+            script: 'dist/main.js',
+            instances: '1', // or a specific number of instances
+            exec_mode: 'cluster', // or 'fork'
+            watch: true,
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+};
--- src/watermark/upload.dto.ts
+++ src/watermark/upload.dto.ts
@@ -5,7 +5,7 @@ export class UploadDto {
     @ApiProperty({ type: 'string', format: 'binary' })
     image: any;
 
-    @ApiProperty({ description: 'Text to be added as watermark' })
-    @IsString()
-    text: string;
+    // @ApiProperty({ description: 'Text to be added as watermark' })
+    // @IsString()
+    // text: string;
 }
